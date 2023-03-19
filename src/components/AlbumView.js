099b6f8ffcbf74b38d1cc2cbcdecca4f54c29c22
Zane Matero <materozane@gmail.com>
2023-03-19 15:55:40 -0700

    artist/album views and functioning links

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,16 +1,36 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function AlbumView() {
-    const [ albumData, setAlbumData ] = useState([])
+    const [ albumData, setAlbumData ] = useState({})
+    const [ songData, setSongData ] = useState ([])
     const { id } = useParams();
 
+    useEffect(() => {
+        fetch(`http://localhost:4000/song/${id}`)
+        .then(response => response.json())
+        .then(({results}) => {
+            console.log(results)
+            setAlbumData(results.shift())
+            setSongData(results)
+        })
+    },[id]);
+    
     return (
         <div>
-            <p>Album Data Goes Here!</p>
-        </div>
+        <Link to={"/"}>Home</Link>
+        <h1>{albumData.collectionName}</h1>
+        <h2>By <Link to={`/../artist/${albumData.artistId}`}>{albumData.artistName}</Link></h2>
+        <h2>{albumData.primaryGenreName}</h2>
+        <h3>Songs:</h3>
+        <ul>
+            {songData.map((song) => {
+                return <li>{song.trackName}</li>
+            })}
+        </ul>
+    </div>
     )
 }
 
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,13 +1,34 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
 import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function ArtistView() {
-    const [ artistData, setArtistData ] = useState([])
+    const [albumData, setAlbumData] = useState([])
+    const [aristData, setArtistData] = useState({})
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/album/${id}`)
+        .then(response => response.json())
+        .then(({results}) => {
+            console.log(results)
+            setArtistData(results.shift())
+            setAlbumData(results)
+        })
+    },[id]);
 
     return (
         <div>
-            <p>Artist Data Goes Here!</p>
+            <Link to={"/"}>Home</Link>
+            <h1>{aristData.artistName}</h1>
+            <h2>{aristData.primaryGenreName}</h2>
+            <h3>Albums:</h3>
+            <ul>
+                {albumData.map((album) => {
+                    return <Link to={`/album/${album.collectionId}`}><li>{album.collectionName}</li></Link>
+                })}
+            </ul>
         </div>
     )
 }
